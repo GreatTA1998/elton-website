acff7e0691faa5353b77c98291c1e7b2b87a8586
Elton Lin <eltonlin1998@gmail.com>
2022-02-19 01:10:47 -0500

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<h1>Personal Blog</h1>
+<p>
+  I write essays, make videos and record music here. Welcome. 
+</p>
+
+<!-- TODO: 
+  Keep it dead simple for now
+  - Every essay is a Firestore doc
+    - Dating (disclaimer about essays I publish)
+    - life management: i.e. tidying, planning, "having a life", physical health, diet, exercise, mental health
+    - Practice journals: piano, sports etc.
+    - Healthy mindsets for looking good, suffering of others (don't be a weeping doctor), opportunity impact
+  - Upload to Soundcloud and just put links here 
+  - Upload to YouTube and just link here
+  - Links to my projects i.e. project calendar, life sanctuary and explain.mit.edu
+
+  Explore: 
+    - A visual map to organize the essays
+    - A novel way for people to engage with my essays i.e. a chat message / thread for each essay, have to use real identity to post and reply
+-->
+<main>
+  <Tiptap/>
+</main>
+
+<script>
+  import Tiptap from '$lib/Tiptap.svelte'
+</script>
+
