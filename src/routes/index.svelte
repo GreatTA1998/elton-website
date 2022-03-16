a5a9d60c2d269d78a357b5186b15735f5d38745b
Elton Lin <eltonlin1998@gmail.com>
2022-03-16 01:18:25 -0400

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,34 @@
-<h1>Personal Blog</h1>
 <p>
-  I write essays, make videos and record music here. Welcome. 
+  Welcome! Here's everything I work on / enjoy doing:
 </p>
 
-<!-- TODO: 
+Web apps:
+<ul style="margin-top: 4px;">
+  <li><a target="_blank" href="https://explain.mit.edu">explain.mit.edu</a></li>
+  <li><a target="_blank" href="https://organize-life.vercel.app">organize-life.vercel.app</a></li>
+  <li><a target="_blank" href="https://zen-messenger.vercel.app">zen-messenger.vercel.app</a></li>
+</ul>
+
+
+
+Content:
+<ul style="margin-top: 4px;">
+  <li><a target="_blank" href="https://www.youtube.com/channel/UC6SBAAxsJL80L0Wn335S6Rg">Youtube</a></li>
+  <li><a target="_blank" href="https://twitter.com/verypopularguy">Twitter</a></li>
+</ul>
+
+Social Media: 
+<ul style="margin-top: 4px;">
+  <li><a target="_blank" href="https://www.facebook.com/eltonlin1998/">Facebook</a></li>
+  <li><a target="_blank" href="https://www.linkedin.com/in/elton-lin/">LinkedIn</a></li>
+</ul>
+
+<img src="david-goggins.png" alt="David Goggins running" width="600"/>
+
+<div>
+  <audio controls="true" src="ghost-in-a-flower.mp3" type="audio/mp3"></audio>
+</div>
+  <!-- TODO: 
   Keep it dead simple for now
   - Every essay is a Firestore doc
     - Dating (disclaimer about essays I publish)
@@ -19,10 +44,10 @@
     - A novel way for people to engage with my essays i.e. a chat message / thread for each essay, have to use real identity to post and reply
 -->
 <main>
-  <Tiptap/>
+  <!-- <Tiptap/> -->
 </main>
 
 <script>
-  import Tiptap from '../TipTap.svelte'
+  // import Tiptap from '../TipTap.svelte'
 </script>
 
